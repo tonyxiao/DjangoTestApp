04d8caabbe2df9a3b25f9b27e88ffdc3a2850c9b
Tony Xiao <tonyx.ca@gmail.com>
2012-06-09 07:15:45 -0700

    Added django admin site to the polls sample app

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,8 @@
+import os
+
 # Django settings for mysite project.
 
+PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -109,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -119,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
